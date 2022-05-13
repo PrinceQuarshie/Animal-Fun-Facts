b226693ec7a850a8809b539da2fe0b3e73fdf871
PrinceQuarshie <prince.quarshie@amalitech.org>
2022-05-13 16:27:47 +0000

    displayInfo| ReactDOM rendering

--- app.js
+++ app.js
@@ -24,3 +24,27 @@ for (const animal in animals) {
     />
   );
 }
+
+
+function displayFact(e) {
+  const selectedAnimal = e.target.alt;
+  const animalInfo = animals[selectedAnimal];
+  const optionIndex = Math.floor(Math.random() * animalInfo.facts.length);
+
+  const funFact = animalInfo.facts[optionIndex];
+  document.getElementById("fact").innerHTML = funFact;
+}
+
+const animalFacts = (
+  <div>
+    <h1>{title || "Click an animal for fun fact"}</h1>;
+    {showBackground && background}
+    <p id="fact"></p>
+    <div className="animals">{images}</div>
+  </div>
+);
+
+ReactDOM.render(animalFacts, document.getElementById("root"));
+
+
+
