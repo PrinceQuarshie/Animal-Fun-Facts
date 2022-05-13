5282b278fa541b1da92585759f7ae5a31ec01b5e
PrinceQuarshie <prince.quarshie@amalitech.org>
2022-05-13 16:26:13 +0000

    background| image

--- app.js
+++ app.js
@@ -2,3 +2,25 @@ import { animals } from "./animals";
 import React from "react";
 import ReactDOM from "react-dom";
 
+const title = "";
+
+const showBackground = true;
+
+const background = (
+  <img className="background" alt="alt" src="/images/ocean.jpg" />
+);
+
+const images = [];
+for (const animal in animals) {
+  images.push(
+    <img
+      key={animal}
+      className="animal"
+      alt={animal}
+      src={animals[animal].image}
+      ariaLabel={animal}
+      role="button"
+      onClick={displayFact}
+    />
+  );
+}
